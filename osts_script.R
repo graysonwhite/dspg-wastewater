0e396557aa5349f979571b6ddda046355e54f880
Grayson White <graysonwhite13@gmail.com>
2020-06-16 15:37:45 -0700

    add google drive data and clean

---
+++ osts_script.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(readxl)
+osts_municipalities <- read_excel("christine-google-drive-data/Oregon Sewage Treatment Systems.xlsx", 
+                                  sheet = "Municipalities")
+
+osts_non_municipalities <- read_excel("christine-google-drive-data/Oregon Sewage Treatment Systems.xlsx", 
+                                      sheet = "non municipalities")
+
+osts_municipalities <- osts_municipalities %>%
+  mutate(municipality = "yes")
+
+osts_non_municipalities <- osts_non_municipalities %>%
+  mutate(municipality = "no")
+
+osts <- left_join(osts_municipalities, osts_non_municipalities)
+
+saveRDS(osts, "osts.rds")
+
