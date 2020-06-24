adb5a3e78d9d696d33679a1197d365492265825e
Grayson White <graysonwhite13@gmail.com>
2020-06-24 14:44:25 -0700

    add survey subset

--- scripts/survey-script.R
+++ scripts/survey-script.R
@@ -11,4 +11,8 @@ saveRDS(survey_2017, "data/cleaned-and-or-rds/survey_2017.rds")
 
 survey_2019 <- read_excel("data/raw/Water Rates Survey 2019_November 19, 2019_08.19.xlsx", 
                           sheet = "Cleaned")
-saveRDS(survey_2019, "data/cleaned-and-or-rds/survey_2019.rds")
+
+survey_2019_select <- survey_2019[, c(1:4, 27,71, 137:162)]
+
+# overwrites data file with file called `survey_2019` in your global environment
+# saveRDS(survey_2019, "data/cleaned-and-or-rds/survey_2019.rds")
