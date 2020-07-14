17fab998af930bc2e926e3e7753ca0b33c697a58
Grayson White <graysonwhite13@gmail.com>
2020-07-14 15:03:25 -0700

    add plotly interactivity

--- wwtp-cost-app/app.R
+++ wwtp-cost-app/app.R
@@ -11,15 +11,13 @@ library(plotly)
 working_df <- read_csv("working_df.csv")
 
 # UI
-
 ui <- navbarPage(
     theme = shinytheme("flatly"),
     title = "Visualizing Data of WWTPs in rural Oregon",
     tabPanel(
         "tab 1: basic example tab",
         mainPanel(
-            plotOutput("map"),
-            textOutput("wd")
+            plotlyOutput("map")
         )
         ),
     tabPanel(
@@ -36,21 +34,17 @@ ui <- navbarPage(
 
 # Server
 server <- function(input, output) {
-    df_sf <- st_as_sf(working_df, coords = c("Longitude", "Latitude"), crs = "+proj=longlat +datum=WGS84")
+    df_sf <- st_as_sf(working_df, coords = c("Longitude", "Latitude"), crs = 4326)
     OR_sf <- us_boundaries(type = "state", states = "OR")
     p1 <- ggplot() +
         geom_sf(data = OR_sf, fill = "#009474") +
-        geom_sf(data = df_sf, color = "#41476b") +
+        geom_point(data = working_df, aes(label = Common_Name, x = Longitude, y = Latitude)) +
         coord_sf() +
         theme_minimal() +
         labs(title = "Wastewater Facilities in Oregon")
 
-    output$map <- renderPlot({
-        # ggplotly(p1, width = 575, height = 425)
-        p1
-    })
-    output$wd <- renderText({
-        getwd()
+    output$map <- renderPlotly({
+        ggplotly(p1)
     })
 }
 
